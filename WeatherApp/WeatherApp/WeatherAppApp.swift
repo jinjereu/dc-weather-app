42c41e60ca1c5fb17da6faab29e797f010cf82b3
ingridsilapan <ingridsilapan@gmail.com>
2022-01-12 11:15:47 +1300

    Add app icons

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Ingrid Silapan on 12/01/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
